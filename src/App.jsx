7dd5bc339f9aeedfa8b4d5a95b71cd33fdba72f1
Maya Eid <mayabaseleid@gmail.com>
2024-12-16 22:42:51 +0200

    frontend: Reset default margin and padding for html and body

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RolePage from "./pages/auth/RolePage";
+import "./styles/fonts.css";
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/role" element={<RolePage />}/>
+        <Route path="/role" element={<RolePage />} />
       </Routes>
     </BrowserRouter>
   );
