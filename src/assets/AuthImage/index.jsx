0e9757f471f48a082564ae5e11f0d8dc28f1baf2
Maya Eid <mayabaseleid@gmail.com>
2024-12-20 20:25:45 +0200

    frontend: Style the image to have the same height as the form container

--- src/assets/AuthImage/index.jsx
+++ src/assets/AuthImage/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import authImage from "./authImage.png";
 
 const AuthImage = () => {
-  return <img src={authImage} alt="Auth" />;
+  return <img src={authImage} alt="Auth" width="100%" />;
 };
 
 export default AuthImage;
