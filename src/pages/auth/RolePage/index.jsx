34650fbddbfb79d141b06212eaf705699ab0f25e
Maya Eid <mayabaseleid@gmail.com>
2024-12-20 20:35:06 +0200

    frontend: Remove input fields from Role Page

--- src/pages/auth/RolePage/index.jsx
+++ src/pages/auth/RolePage/index.jsx
@@ -7,14 +7,7 @@ const RolePage = () => {
   return (
     <div className="main-container">
       <div className="form-container">
-        <div className="inputs-container">
-        <div className="name-container">
-          <Input placeholder="First Name" />
-          <Input placeholder="Last Name" />
-        </div>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        </div>
+        
       </div>
       <div className="image-container">
         <AuthImage />
