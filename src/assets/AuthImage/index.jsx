5b056b6f0332db4259f87d9d39d934c1f092613a
Maya Eid <mayabaseleid@gmail.com>
2024-12-20 19:43:29 +0200

    frontend: Create AuthImage component to display the image in authentication pages

---
+++ src/assets/AuthImage/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import authImage from "./authImage.png";
+
+const AuthImage = () => {
+  return <img src={authImage} alt="Auth Image" />;
+};
+
+export default AuthImage;
