e8b533270da36e0a8db04e2d1ea81ef767467141
Maya Eid <mayabaseleid@gmail.com>
2024-12-20 22:51:02 +0200

    frontend: Add another button for user role

--- src/assets/icons/UserRoleIcon/index.jsx
+++ src/assets/icons/UserRoleIcon/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import user from "./user.png";
 
 const UserRoleIcon = () => {
-  return <img src={user} alt="User" />;
+  return <img src={user} alt="User" width="100%"/>;
 };
 
 export default UserRoleIcon;
--- src/pages/auth/RolePage/index.jsx
+++ src/pages/auth/RolePage/index.jsx
@@ -5,6 +5,7 @@ import AuthImage from "../../../assets/images/AuthImage";
 import LogoNoText from "../../../assets/images/LogoNoText";
 import BigButton from "../../../components/base/BigButton";
 import GardenerIcon from "../../../assets/icons/GardenerIcon";
+import UserRoleIcon from "../../../assets/icons/UserRoleIcon";
 
 const RolePage = () => {
   return (
@@ -13,6 +14,11 @@ const RolePage = () => {
         <div className="logo-container">
           <LogoNoText />
           <h1>Sign Up</h1>
+          <BigButton
+            icon={<UserRoleIcon />}
+            title="As a Gardener"
+            subtitle="I'm here to sell and care for my plants"
+          />
           <BigButton
             icon={<GardenerIcon />}
             title="As a Gardener"
