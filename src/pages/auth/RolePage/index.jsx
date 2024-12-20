bb4dfd9950ee2bd0b76e6685e9a6e7caf0dac29d
Maya Eid <mayabaseleid@gmail.com>
2024-12-20 20:45:25 +0200

    frontend: Adjust the height of the logo image

--- src/pages/auth/RolePage/index.jsx
+++ src/pages/auth/RolePage/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Input from "../../../components/base/Input";
 import "./style.css";
 import AuthImage from "../../../assets/AuthImage";
+import LogoNoText from "../../../assets/LogoNoText";
 
 const RolePage = () => {
   return (
     <div className="main-container">
       <div className="form-container">
-        
+        <LogoNoText />
       </div>
       <div className="image-container">
         <AuthImage />
