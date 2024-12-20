933335cd4a3f72261cc0eb9d3c8302336793aa04
Maya Eid <mayabaseleid@gmail.com>
2024-12-21 00:18:49 +0200

    frontend: Change font family and capitalization in white button

--- src/components/base/WhiteButton/index.jsx
+++ src/components/base/WhiteButton/index.jsx
@@ -1,6 +1,7 @@
 import { ArrowForward } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import React from "react";
+import '../../../styles/fonts.css'
 
 const WhiteButton = ({ label }) => {
   return (
@@ -12,6 +13,8 @@ const WhiteButton = ({ label }) => {
         color: 'black', 
         borderRadius: '10px', 
         width: '250px',
+        fontFamily: 'Proxima Nova Regular',
+        textTransform: 'capitalize',
         '&:hover': {
           backgroundColor: '#f0f0f0',
         },
