67e2fdb2693a6d6a27d50e0afb7618ec8e02a553
Maya Eid <mayabaseleid@gmail.com>
2024-12-21 02:00:55 +0200

    frontend: Apply same styles from role page to sign up page

--- src/components/base/BigButton/index.jsx
+++ src/components/base/BigButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 
 const BigButton = ({ icon, title, subtitle, active, onClick }) => {
--- src/pages/auth/SignUpGardener/index.jsx
+++ src/pages/auth/SignUpGardener/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoNoText from "../../../assets/images/LogoNoText";
 import AuthImage from "../../../assets/images/AuthImage";
+import "./style.css";
 
 const SignUpGardener = () => {
   return (
@@ -9,6 +10,7 @@ const SignUpGardener = () => {
         <div className="logo-container">
           <LogoNoText />
           <h1>Sign Up</h1>
+          <p>Join us today to start exploring and caring for your favorite flowers!</p>
         </div>
       </div>
       <div className="image-container">
