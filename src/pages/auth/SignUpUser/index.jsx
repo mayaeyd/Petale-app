3cd15e15b0bf580249afe9879e7b1a0251f33349
Maya Eid <mayabaseleid@gmail.com>
2024-12-21 20:25:26 +0200

    frontend: Setup the sign up page for the user

---
+++ src/pages/auth/SignUpUser/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import LogoNoText from "../../../assets/images/LogoNoText";
+import AuthImage from "../../../assets/images/AuthImage";
+import "./style.css";
+import Input from "../../../components/base/Input";
+import WhiteButton from "../../../components/base/WhiteButton";
+
+const SignUpUser = () => {
+  return (
+    <div className="main-container">
+      <div className="form-container">
+        <div className="logo-container">
+          <LogoNoText />
+          <h1>Sign Up</h1>
+          <p>
+            Join us today to start exploring and caring for your favorite
+            flowers!
+          </p>
+        </div>
+
+        <div className="inputs-container">
+          <div className="name-container">
+            <Input placeholder="First Name" type="text" />
+            <Input placeholder="Last Name" type="text" />
+          </div>
+          <Input placeholder="Email" type="email" />
+          <Input placeholder="Password" type="password" />
+          <Input placeholder="Confirm Password" type="password" />
+          <Input placeholder="" type="tel" />
+          <Input placeholder="Location" type="text" />
+          <WhiteButton label="Sign Up" />
+          <p>
+            Already have an account? <a href="/login">Login</a>
+          </p>
+        </div>
+      </div>
+      <div className="image-container">
+        <AuthImage />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpUser;
