02029058fafb18be7b2ca5606e56e3c001deb91e
Maya Eid <mayabaseleid@gmail.com>
2024-12-21 15:59:01 +0200

    frontend: Fix Input component to make the adornment color white

--- src/components/base/Input/index.jsx
+++ src/components/base/Input/index.jsx
@@ -1,63 +1,62 @@
 import React from "react";
-import "./style.css";
 import { InputAdornment, TextField } from "@mui/material";
 
 const Input = ({ placeholder, type }) => {
   return (
-    <>
-      <TextField
-        label={placeholder}
-        variant="outlined"
-        type={type}
-        fullWidth
-        inputProps={{
-          maxLength: type === "tel" ? 8 : undefined, 
-          pattern: type === "tel" ? "[0-9]*" : undefined, 
-        }}
-        sx={{
-          "& .MuiOutlinedInput-root": {
-            backgroundColor: "#ffffff21",
-            borderRadius: "15px",
-            color: "#fff",
-            fontFamily: "Proxima Nova Light",
-            "& input": {
-              color: "#fff",
-              opacity: 1,
-            },
-            "& .MuiOutlinedInput-notchedOutline": {
-              borderColor: "#fff",
-              borderWidth: "2px",
-            },
-            "&:hover": {
-              "& .MuiOutlinedInput-notchedOutline": {
-                borderColor: "#fff",
-              },
-            },
-            "&.Mui-focused": {
-              "& .MuiOutlinedInput-notchedOutline": {
-                borderColor: "#fff",
-                borderWidth: "3px",
-              },
-            },
-          },
-          "& .MuiInputLabel-outlined": {
-            color: "#fff",
-            fontFamily: "Proxima Nova Thin",
-            opacity: 1,
-            "&.Mui-focused": {
-              color: "#fff",
-            },
-          },
-        }}
-        InputProps={{
+    <TextField
+      label={placeholder}
+      variant="outlined"
+      type={type}
+      fullWidth
+      slotProps={{
+        htmlInput: {
+          maxLength: type === "tel" ? 8 : undefined,
+          inputMode: type === "tel" ? "numeric" : undefined,
+        },
+        input: {
           startAdornment: type === "tel" && (
-            <InputAdornment position="start">
+            <InputAdornment
+              position="start"
+              sx={{
+                color: "#fff",
+                fontFamily: "Proxima Nova Light",
+              }}
+            >
               🇱🇧 +961
             </InputAdornment>
-          )
-        }}
-      />
-    </>
+          ),
+        },
+      }}
+      sx={{
+        "& .MuiOutlinedInput-root": {
+          backgroundColor: "#ffffff21",
+          borderRadius: "15px",
+          fontFamily: "Proxima Nova Light",
+          "& input": {
+            color: "#fff",
+            opacity: 1,
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fff",
+            borderWidth: "2px",
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fff",
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#fff",
+            borderWidth: "3px",
+          },
+        },
+        "& .MuiInputLabel-outlined": {
+          color: "#fff",
+          fontFamily: "Proxima Nova Thin",
+          "&.Mui-focused": {
+            color: "#fff",
+          },
+        },
+      }}
+    />
   );
 };
 
