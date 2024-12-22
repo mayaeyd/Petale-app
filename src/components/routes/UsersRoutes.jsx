2e3443f0f43d21a9a5a2a24d2b69909025251b1a
Maya Eid <mayabaseleid@gmail.com>
2024-12-22 23:43:31 +0200

    frontend: Add protected route logic for users with role-based access control and redirection

--- src/components/routes/UsersRoutes.jsx
+++ src/components/routes/UsersRoutes.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-const GardenersRoutes = () => {
+const UsersRoutes = () => {
   const [check, setCheck] = useState(false);
 
   const { user, isLoggedIn, token } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    if (user && isLoggedIn && token && user.role === "gardener") {
+    if (user && isLoggedIn && token && user.role === "user") {
       setCheck(true);
     } else {
       setCheck(false);
@@ -18,4 +18,4 @@ const GardenersRoutes = () => {
   return check ? <Outlet /> : <Navigate to={"/"} />;
 };
 
-export default GardenersRoutes;
+export default UsersRoutes;
