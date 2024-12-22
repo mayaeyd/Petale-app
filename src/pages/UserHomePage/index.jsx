579cf370eac75d013555eb6804f40b059bdceb9e
Maya Eid <mayabaseleid@gmail.com>
2024-12-22 23:05:35 +0200

    frontend: Initialize UserHomePage component and add basic navbar in it

---
+++ src/pages/UserHomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserNavbar from '../../components/common/UserNavbar'
+
+const UserHomePage = () => {
+  return (
+    <div>
+      <UserNavbar />
+    </div>
+  )
+}
+
+export default UserHomePage
