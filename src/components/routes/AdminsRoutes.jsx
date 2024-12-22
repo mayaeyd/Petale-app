1c72d8a75405d81afccff5394462e46accc97b58
Maya Eid <mayabaseleid@gmail.com>
2024-12-22 23:45:07 +0200

    frontend: Add protected route logic for admins with role-based access control and redirection

---
+++ src/components/routes/AdminsRoutes.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const AdminsRoutes = () => {
+  const [check, setCheck] = useState(false);
+
+  const { user, isLoggedIn, token } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user && isLoggedIn && token && user.role === "admin") {
+      setCheck(true);
+    } else {
+      setCheck(false);
+    }
+  }, [user, isLoggedIn, token]);
+
+  return check ? <Outlet /> : <Navigate to={"/"} />;
+};
+
+export default AdminsRoutes;
