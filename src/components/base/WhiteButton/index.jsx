d6e5dc3866072915a05d58ac9c42c99eecaa5152
Maya Eid <mayabaseleid@gmail.com>
2024-12-22 14:22:18 +0200

    frontend: Add an onClick prop to WhiteButton component

--- src/components/base/WhiteButton/index.jsx
+++ src/components/base/WhiteButton/index.jsx
@@ -3,17 +3,18 @@ import { Button } from "@mui/material";
 import React from "react";
 import "../../../styles/fonts.css";
 
-const WhiteButton = ({ label }) => {
+const WhiteButton = ({ label, onClick }) => {
   return (
     <Button
       variant="contained"
       endIcon={<ArrowForward />}
       fullWidth
+      onClick={() => onClick()}
       sx={{
         backgroundColor: "white",
         color: "black",
         borderRadius: "10px",
-        marginTop:"10px",
+        marginTop: "10px",
         height: "35px",
         fontFamily: "Proxima Nova Regular",
         textTransform: "capitalize",
