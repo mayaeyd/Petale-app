e88104bfa746d157f3b286005adbf486eba334ef
Maya Eid <mayabaseleid@gmail.com>
2024-12-23 16:33:29 +0200

    frontend: Fetch getSelf API on every reload and add role-based authorization checks

--- src/components/routes/UsersRoutes.jsx
+++ src/components/routes/UsersRoutes.jsx
@@ -1,47 +1,25 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import axios from "axios";
 
 const UsersRoutes = () => {
-  const [check, setCheck] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [check, setCheck] = useState(true);
+
+  const getAuthUser = async () => {
+    const { data } = await axios.get("http://127.0.0.1:8080/auth/getSelf", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    setCheck(data.user.role === "user");
+  };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const token = localStorage.getItem("token");
-
-        if (!token) {
-          throw new Error("No token found");
-        }
-
-        const { data } = await axios.post("http://localhost:8080/auth/getSelf", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (data.user.role === "user") {
-          setCheck(true);
-        } else {
-          setCheck(false);
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-        setCheck(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUser();
+    getAuthUser();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>; // Optionally add a spinner or loader
-  }
-
-  return check ? <Outlet /> : <Navigate to="/" />;
+  return check ? <Outlet /> : <Navigate to={"/"} />;
 };
 
 export default UsersRoutes;
