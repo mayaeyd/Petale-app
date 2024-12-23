fbc9299f079c8e79fac2ec35b7d4bc8ed175a193
Maya Eid <mayabaseleid@gmail.com>
2024-12-23 21:10:47 +0200

    frontend: Divide the footer into three sections

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -6,14 +6,19 @@ import "./style.css";
 const Footer = () => {
   return (
     <footer>
-      <div className="logo-subtitle">
-        <div className="subtitle">
-          <h2>PÉTALE</h2>
-          <p>Grow Together</p>
+      <div className="footer-sec-1">
+        <div className="logo-subtitle">
+          <div className="subtitle">
+            <h2>PÉTALE</h2>
+            <p>Grow Together</p>
+          </div>
+          <LogoNoText />
         </div>
-        <LogoNoText />
+        <p>Nurture. Bloom. Connect.</p>
+        <PinkButtonRound label="Buy Flowers" />
       </div>
-      <PinkButtonRound label="Buy Flowers" />
+      <div className="footer-sec-2"></div>
+      <div className="footer-sec-3"></div>
     </footer>
   );
 };
