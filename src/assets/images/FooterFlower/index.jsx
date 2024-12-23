53fcebe3989a684c3481037a7624feb05a775dc3
Maya Eid <mayabaseleid@gmail.com>
2024-12-23 23:44:34 +0200

    frontend: Create FooterFlower component to use image

---
+++ src/assets/images/FooterFlower/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import footerFlower from "./footerFlower.png";
+
+const FooterFlower = () => {
+  return <img src={footerFlower} alt="Flower" />;
+};
+
+export default FooterFlower;
