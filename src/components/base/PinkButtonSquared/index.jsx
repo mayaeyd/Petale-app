95bf7df18a7467546bae1667529c86b7bb4c5649
Maya Eid <mayabaseleid@gmail.com>
2024-12-23 22:41:21 +0200

    frontend: Create pink squared button to place in footer

---
+++ src/components/base/PinkButtonSquared/index.jsx
@@ -0,0 +1,28 @@
+import { ArrowForward } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React from "react";
+import "../../../styles/fonts.css";
+
+const PinkButtonRound = ({ label, onClick }) => {
+  return (
+    <Button
+      variant="contained"
+      onClick={() => onClick()}
+      fullWidth
+      sx={{
+        backgroundColor: "#F7D6D6",
+        color: "#BE7D86",
+        border: "2px solid #BE7D86",
+        marginTop: "10px",
+        height: "35px",
+        borderRadius: "0px",
+        fontFamily: "Proxima Nova Semibold",
+        fontSize: "1em",
+      }}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default PinkButtonRound;
