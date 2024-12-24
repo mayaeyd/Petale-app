a98a4eee6a7a9b1bbcc66954589ff029a7059edf
Maya Eid <mayabaseleid@gmail.com>
2024-12-24 22:44:03 +0200

    frontend: Import logo in navbar

---
+++ src/components/common/GardenerNavbar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LogoText from "../../../assets/images/LogoText"
+
+const GardenerNavbar = () => {
+  return <nav>
+    <div>
+        <LogoText />
+    </div>
+  </nav>;
+};
+
+export default GardenerNavbar;
