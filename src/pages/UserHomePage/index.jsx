ff5b13b0b04f5db23060366560844421c638b9f8
Maya Eid <mayabaseleid@gmail.com>
2024-12-24 21:30:16 +0200

    frontend: Import images in MasonryHero component

--- src/pages/UserHomePage/index.jsx
+++ src/pages/UserHomePage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UserNavbar from '../../components/common/UserNavbar'
 import Footer from '../../components/common/Footer'
+import MasonryHero from '../../components/base/MasonryHero'
 
 const UserHomePage = () => {
   return (
     <div>
       <UserNavbar />
+      <MasonryHero />
       <Footer />
     </div>
   )
