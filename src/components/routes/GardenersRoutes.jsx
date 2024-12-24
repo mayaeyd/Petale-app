2cd9fe6dca2c225aa483b6356c1527020c740d91
Maya Eid <mayabaseleid@gmail.com>
2024-12-24 23:04:18 +0200

    frontend: Add try-catch block in GardenersRoutes

--- src/components/routes/GardenersRoutes.jsx
+++ src/components/routes/GardenersRoutes.jsx
@@ -3,21 +3,29 @@ import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const GardenersRoutes = () => {
-  const [check, setCheck] = useState(false);
+  const [check, setCheck] = useState(null);
 
   const getAuthUser = async () => {
-    const { data } = await axios.get("http://127.0.0.1:8080/auth/getSelf", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
-
-    setCheck(data.user.role === "gardener");
+    try {
+      const { data } = await axios.get("http://127.0.0.1:8080/auth/getSelf", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      setCheck(data.user.role === "gardener");
+    } catch (error) {
+      console.error("Error authenticating user:", error);
+      setCheck(false);
+    }
   };
 
   useEffect(() => {
     getAuthUser();
   }, []);
+
+  if (check === null) {
+    return;
+  }
 
   return check ? <Outlet /> : <Navigate to={"/"} />;
 };
