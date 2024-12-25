3d24cce9250752d78a680404f0a1f82ab8146291
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 20:57:06 +0200

    frontend: Dispatch fetchPlants on load of growing plants page

--- src/pages/GrowingPlantsPage/index.jsx
+++ src/pages/GrowingPlantsPage/index.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GardenerNavbar from "../../components/common/GardenerNavbar";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPlants } from "../../redux/slices/plantsSlice";
+import { CircularProgress } from "@mui/material";
 
 const GrowingPlantsPage = () => {
+  const { plants, loading, error } = useSelector((state) => state.plants);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPlants());
+  }, []);
+
+  if (loading) {
+    return <CircularProgress color="success" />;
+  }
+
+  if (error) {
+    console.error(error);
+  }
+
+  console.log("Plants data:", plants);
+
   return (
     <div>
       <GardenerNavbar />
