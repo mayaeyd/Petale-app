3a24342aebca7635511b28d9a3d389b922fe19af
Maya Eid <mayabaseleid@gmail.com>
2024-12-26 00:42:20 +0200

    frontend: Add onClick handler on growing plant card and navigate to relative details page

--- src/components/common/GrowingPlantCard/index.jsx
+++ src/components/common/GrowingPlantCard/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
-const GrowingPlantCard = ({ name, date }) => {
-  return <div className="growing-plt-card">
-    <h2>{name}</h2>
-    <p>{date}</p>
-  </div>;
+const GrowingPlantCard = ({ name, date, id }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/gardener/plants/${id}`);
+  };
+
+  return (
+    <div className="growing-plt-card" onClick={handleClick}>
+      <h2>{name}</h2>
+      <p>{date}</p>
+    </div>
+  );
 };
 
 export default GrowingPlantCard;
