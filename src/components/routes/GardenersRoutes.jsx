b63d52dc393e20cac39811d022be77b103d53097
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 13:54:27 +0200

    frontend: Update GardenersRoutes to validate user role using Redux state and improve conditional rendering

--- src/components/routes/GardenersRoutes.jsx
+++ src/components/routes/GardenersRoutes.jsx
@@ -1,30 +1,17 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const GardenersRoutes = () => {
+  const { user } = useSelector((state) => state.auth);
   const [check, setCheck] = useState(null);
-
-  const getAuthUser = async () => {
-    try {
-      const { data } = await axios.get("http://127.0.0.1:8080/auth/getSelf", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      setCheck(data.user.role === "gardener");
-    } catch (error) {
-      console.error("Error authenticating user:", error);
-      setCheck(false);
-    }
-  };
-
-  useEffect(() => {
-    getAuthUser();
-  }, []);
 
   if (check === null) {
     return;
+  }
+
+  if (user) {
+    setCheck(user.role === "gardener");
   }
 
   return check ? <Outlet /> : <Navigate to={"/"} />;
