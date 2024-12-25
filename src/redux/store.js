f05104361d7c1332ac63e9a85cfff1dcb08d30f2
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 20:44:22 +0200

    frontend: Add the plants reducer in the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import plantsReducer from "./slices/plantsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    plants: plantsReducer,
   },
 });
 
