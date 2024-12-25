f8d8b10131286655da7e841fe1ac8d9215d54c05
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 16:14:51 +0200

    frontend: Add plants nav links in navbar

--- src/components/common/GardenerNavbar/index.jsx
+++ src/components/common/GardenerNavbar/index.jsx
@@ -18,6 +18,7 @@ const GardenerNavbar = () => {
     <nav className="gardener-navbar">
       <div>
         <LogoText />
+
         <div className="gardener-info">
           <svg
             width="34"
@@ -26,13 +27,73 @@ const GardenerNavbar = () => {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <circle cx="17" cy="17" r="17" fill="#D9D9D9" style={{zIndex:100, color:"white"}} />
+            <circle cx="17" cy="17" r="17" fill="#D9D9D9" />
           </svg>
           <div className="gardener-name">
             <h3>John Doe</h3>
             <p>John Doe Co.</p>
           </div>
         </div>
+
+        <div className="plants-nav-links">
+          <a className="gardener-nav-link">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-activity"
+            >
+              <path d="M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2" />
+            </svg>
+            <p>Growing Plants</p>
+          </a>
+          <a className="gardener-nav-link">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-clipboard-check"
+            >
+              <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
+              <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+              <path d="m9 14 2 2 4-4" />
+            </svg>
+            <p>Posted Plants</p>
+          </a>
+          <a className="gardener-nav-link">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-coins"
+            >
+              <circle cx="8" cy="8" r="6" />
+              <path d="M18.09 10.37A6 6 0 1 1 10.34 18" />
+              <path d="M7 6h1v4" />
+              <path d="m16.71 13.88.7.71-2.82 2.82" />
+            </svg>
+            <p>Sold Plants</p>
+          </a>
+        </div>
+        <div className="personal-nav-links"></div>
       </div>
     </nav>
   );
