acac3ea821ceacbcd46fd1efe887d1a6e575a87d
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 21:30:26 +0200

    frontend: Add button to add plant in growing plants page and display plant name for testing

--- src/pages/GrowingPlantsPage/index.jsx
+++ src/pages/GrowingPlantsPage/index.jsx
@@ -3,6 +3,9 @@ import GardenerNavbar from "../../components/common/GardenerNavbar";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPlants } from "../../redux/slices/plantsSlice";
 import { CircularProgress } from "@mui/material";
+import PinkButtonRound from "../../components/base/PinkButtonRound";
+import AddIcon from "@mui/icons-material/Add";
+import "./style.css";
 
 const GrowingPlantsPage = () => {
   const { plants, loading, error } = useSelector((state) => state.plants);
@@ -25,6 +28,12 @@ const GrowingPlantsPage = () => {
   return (
     <div>
       <GardenerNavbar />
+      <div className="btn-container">
+        <PinkButtonRound label="Add Plant" endIcon={<AddIcon />} />
+        {plants.map((plant, index) => (
+          <h1>{plant.scientificName}</h1>
+        ))}
+      </div>
     </div>
   );
 };
