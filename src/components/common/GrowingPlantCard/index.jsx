2b93ae1ec13fc2d7ee0a4b56c364fa019de077c1
Maya Eid <mayabaseleid@gmail.com>
2024-12-25 23:27:49 +0200

    frontend: Add plant name and planted date in card

--- src/components/common/GrowingPlantCard/index.jsx
+++ src/components/common/GrowingPlantCard/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
-const GrowingPlantCard = ({name, date}) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const GrowingPlantCard = ({ name, date }) => {
+  return <div className="growing-plt-card">
+    <h2>{name}</h2>
+    <p>{date}</p>
+  </div>;
+};
 
-export default GrowingPlantCard
+export default GrowingPlantCard;
