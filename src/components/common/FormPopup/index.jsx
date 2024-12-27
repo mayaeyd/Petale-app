40a40f2d14789afc2416c7e0e11b3d2f632443be
Maya Eid <mayabaseleid@gmail.com>
2024-12-27 15:16:14 +0200

    frontend: Style the form popup

--- src/components/common/FormPopup/index.jsx
+++ src/components/common/FormPopup/index.jsx
@@ -1,15 +1,17 @@
 import {
-  Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
   Slide,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import React, { forwardRef, Fragment, useState } from "react";
 import PinkButtonRound from "../../base/PinkButtonRound";
 import AddIcon from "@mui/icons-material/Add";
+import "../../../styles/fonts.css";
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -25,6 +27,7 @@ const FormPopup = () => {
   const handleClose = () => {
     setOpen(false);
   };
+  
 
   return (
     <Fragment>
@@ -39,15 +42,47 @@ const FormPopup = () => {
         TransitionComponent={Transition}
         keepMounted
         onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
+        aria-labelledby="responsive-dialog-title"
+        sx={{
+          "& .MuiPaper-root": {
+            border: "2px solid #BE7D86", // Dialog container border
+            borderRadius: "12px", // Rounded corners
+            backgroundColor: "white",
+            padding: "16px", // Padding inside the dialog
+          },
+        }}
       >
-        <DialogTitle></DialogTitle>
-        <DialogContent>
+        <DialogTitle
+          sx={{
+            color: "#BE7D86",
+            fontFamily: "Cormorant Semibold",
+            fontWeight: "500",
+            fontSize: "2em",
+            textAlign: "center",
+          }}
+          id="responsive-dialog-title"
+        >
+          Add Plant
+        </DialogTitle>
+        <DialogContent
+          sx={{
+            "& .MuiDialogContentText-root": {
+              fontSize: "16px", // Content text size
+              color: "gray", // Content text color
+            },
+          }}
+        >
           <DialogContentText id="alert-dialog-slide-description">
-            
+            Customize the content here to guide users.
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
+        <DialogActions
+          sx={{
+            display: "flex",
+            justifyContent: "space-between", // Space between buttons
+            padding: "8px 16px", // Adjust padding
+          }}
+        >
           <PinkButtonRound onClick={handleClose} label="Close" />
           <PinkButtonRound onClick={handleClose} label="Add" />
         </DialogActions>
