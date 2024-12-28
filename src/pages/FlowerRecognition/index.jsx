25041c28f48a7a9e106a2787b9331dc29433ea65
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 01:24:42 +0200

    frontend: Add images to FlowerRecognition page

--- src/pages/FlowerRecognition/index.jsx
+++ src/pages/FlowerRecognition/index.jsx
@@ -3,16 +3,23 @@ import "./style.css";
 import UserNavbar from "../../components/common/UserNavbar";
 import Footer from "../../components/common/Footer";
 import FlowerUpload from "../../components/common/FlowerUpload";
+import BackgroundFlower from "../../assets/images/BackgroundFlower";
 
 const FlowerRecognition = () => {
   return (
-    <>
+    <div className="flower-recoganition-div">
       <UserNavbar />
+      <div className="flower-1">
+        <BackgroundFlower />
+      </div>
       <div className="file-upload-container">
         <FlowerUpload />
       </div>
+      <div className="flower-2">
+        <BackgroundFlower />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
