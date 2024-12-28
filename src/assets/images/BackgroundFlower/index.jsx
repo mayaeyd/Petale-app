0ca97dac6a1613319011a4bb8fcf72ce4f2f75b6
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 01:24:04 +0200

    frontend: Adjust width of background image

--- src/assets/images/BackgroundFlower/index.jsx
+++ src/assets/images/BackgroundFlower/index.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
 import flowerImage from "./flower-5-removebg-preview.png";
 
 const BackgroundFlower = () => {
-  return (
-    <img src={flowerImage} alt='Flower' />
-  )
-}
+  return <img src={flowerImage} alt="Flower" width="700px" />;
+};
 
-export default BackgroundFlower
+export default BackgroundFlower;
