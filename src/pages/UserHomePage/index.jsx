1d3fb58f2a8133b49b2872e1bb41a5fd7e7917c4
Maya Eid <mayabaseleid@gmail.com>
2024-12-28 02:46:40 +0200

    frontend: Fix footer positioning

--- src/pages/UserHomePage/index.jsx
+++ src/pages/UserHomePage/index.jsx
@@ -3,6 +3,8 @@ import UserNavbar from "../../components/common/UserNavbar";
 import Footer from "../../components/common/Footer";
 import MasonryHero from "../../components/base/MasonryHero";
 import "./style.css";
+import PinkButtonRound from "../../components/base/PinkButtonRound";
+import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
 
 const UserHomePage = () => {
   return (
@@ -15,6 +17,8 @@ const UserHomePage = () => {
           <h3>Nurture. Bloom. Connect</h3>
         </div>
       </div>
+      <h1>jhuh</h1>
+      <PinkButtonRound endIcon={<LocalFloristIcon />} label="Know Flower Name by Image" className="pink-btn" />
       <Footer />
     </div>
   );
