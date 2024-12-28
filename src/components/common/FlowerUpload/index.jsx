af97994f232223d7794a886b119b78ea72586f40
Maya Eid <mayabaseleid@gmail.com>
2024-12-28 18:58:41 +0200

    frontend: Structure the file upload container and cadd class names

---
+++ src/components/common/FlowerUpload/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useRef } from "react";
+import { Upload, X } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { predictFlower } from "../../../redux/slices/flowerRecognitionSlice";
+import "./style.css";
+
+const FlowerUpload = () => {
+  const dispatch = useDispatch();
+  const { loading, prediction, confidence } = useSelector(
+    (state) => state.flower
+  );
+
+  return (
+    <div className="upload-container">
+      <h1>Flower Recognition</h1>
+
+      <div className="upload-content">
+        <div className={`upload-area ${isDragOver ? "dragover" : ""}`}>
+          <input type="file" className="file-input" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlowerUpload;
