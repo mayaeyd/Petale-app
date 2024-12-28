f66bd6d40095739b0fd3da4d478c29fe9417ff29
Maya Eid <mayabaseleid@gmail.com>
2024-12-28 18:27:13 +0200

    frontend: Add the flowerRecognitionReducer to configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import plantsReducer from "./slices/plantsSlice";
+import flowerRecognitionReducer from "./slices/flowerRecognitionSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     plants: plantsReducer,
+    flower: flowerRecognitionReducer,
   },
 });
 
