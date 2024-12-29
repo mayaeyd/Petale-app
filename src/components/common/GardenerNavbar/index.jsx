be9b81398c9313eb99873a725c03c08002bd67ea
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 17:15:03 +0200

    frontend: Add route to posted plants page in navbar

--- src/components/common/GardenerNavbar/index.jsx
+++ src/components/common/GardenerNavbar/index.jsx
@@ -63,7 +63,7 @@ const GardenerNavbar = () => {
             <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
             <path d="m9 14 2 2 4-4" />
           </svg>
-          <a>Posted Plants</a>
+          <a href="/gardener/posted-plants">Posted Plants</a>
         </div>
         <div className="gardener-nav-link">
           <svg
