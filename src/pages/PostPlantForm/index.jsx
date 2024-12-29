6c3e6fa608737c31ce66789c898298a00d679bfd
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 18:32:37 +0200

    frontend: Add plant name input in post plant form

--- src/pages/PostPlantForm/index.jsx
+++ src/pages/PostPlantForm/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import GardenerNavbar from "../../components/common/GardenerNavbar";
 import "./style.css";
+import Input from "../../components/base/Input";
 
 const PostPlantForm = () => {
   return (
     <div className="post-plant-form">
       <GardenerNavbar />
       <h1>Post your plant for sale</h1>
+      <div className="post-inputs-container">
+        <Input inputColor="#BE7D86" label="Plant Name" />
+      </div>
     </div>
   );
 };
