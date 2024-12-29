938f61f2842bf4a266c456abf393f6a5cef2de2a
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 23:15:54 +0200

    frontend: Style the TextArea component

--- src/components/base/TextArea/index.jsx
+++ src/components/base/TextArea/index.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import { TextareaAutosize } from "@mui/base/TextareaAutosize";
 import { styled } from "@mui/system";
 
-const TextArea = ({ minRows, minLength, placeholder = "", ...props }) => {
+const StyledTextarea = styled(TextareaAutosize)`
+  box-sizing: border-box;
+  width: 100%;
+  font-family: "Proxima Nova Light";
+  font-size: 0.875rem;
+  line-height: 1.5;
+  padding: 8px 12px;
+  border-radius: 8px;
+  color: #fff;
+  background: #ffffff21;
+  border: 2px solid #be7d86;
+
+  &:hover {
+    border-color: #be7d86;
+  }
+
+  &:focus {
+    border: 3px solid #be7d86;
+    outline: 0;
+  }
+`;
+
+const TextArea = ({ minRows, minLength, placeholder, ...props }) => {
   return (
     <StyledTextarea
       minRows={minRows}
