a1543dc3c19c1e99fc18e83874701726493e87ea
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 17:54:04 +0200

    frontend: Add onClick and endIcon props to post plant button

--- src/pages/PostedPlantsPage/index.jsx
+++ src/pages/PostedPlantsPage/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import GardenerNavbar from "../../components/common/GardenerNavbar";
 import PinkButtonRound from "../../components/base/PinkButtonRound";
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 const PostedPlantsPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <GardenerNavbar />
       <div className="posted-plants-container">
         <div className="btn-container">
-          <PinkButtonRound label="Post Plant" />
+          <PinkButtonRound
+            label="Post Plant"
+            onClick={() => navigate("/gardener/post-plant")}
+            endIcon={<EventAvailableIcon />}
+          />
         </div>
       </div>
     </div>
