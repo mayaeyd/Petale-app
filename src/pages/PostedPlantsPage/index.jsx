08d2eb85530214aaf79eed220bdafdde45cf0565
Maya Eid <mayabaseleid@gmail.com>
2024-12-29 17:35:37 +0200

    frontend: Add post plant button in posted plant page

--- src/pages/PostedPlantsPage/index.jsx
+++ src/pages/PostedPlantsPage/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import GardenerNavbar from '../../components/common/GardenerNavbar'
+import React from "react";
+import GardenerNavbar from "../../components/common/GardenerNavbar";
+import PinkButtonRound from "../../components/base/PinkButtonRound";
+import "./style.css";
 
 const PostedPlantsPage = () => {
   return (
     <div>
       <GardenerNavbar />
+      <div className="posted-plants-container">
+        <PinkButtonRound label="Post Plant" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostedPlantsPage
+export default PostedPlantsPage;
