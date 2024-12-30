a498252abd0dd6bf641df4c93b4abf28d2bd7cd8
Maya Eid <mayabaseleid@gmail.com>
2024-12-30 16:31:19 +0200

    frontend: Add onChange handlers on all inputs and get images and plantType from plantsSlice in PostPlantForm component

--- src/pages/PostPlantForm/index.jsx
+++ src/pages/PostPlantForm/index.jsx
@@ -7,9 +7,12 @@ import MultipleImageUpload from "../../components/common/MultipleImageUpload";
 import DateField from "../../components/base/DateField";
 import TextArea from "../../components/base/TextArea";
 import { useSelector } from "react-redux";
+import PinkButtonRound from "../../components/base/PinkButtonRound";
 
 const PostPlantForm = () => {
   const { images } = useSelector((state) => state.plants);
+  const { plantType } = useSelector((state) => state.plants);
+
   const [plantName, setPlantName] = useState("");
   const [price, setPrice] = useState(0);
   const [harvestedDate, setHarvestedDate] = useState(null);
@@ -20,8 +23,16 @@ const PostPlantForm = () => {
       <GardenerNavbar />
       <h1>Post your plant for sale</h1>
       <div className="post-inputs-container">
-        <Input inputColor="#BE7D86" label="Plant Name" />
-        <Input inputColor="#BE7D86" label="Price" />
+        <Input
+          inputColor="#BE7D86"
+          label="Plant Name"
+          onChange={(e) => setPlantName(e.target.value)}
+        />
+        <Input
+          inputColor="#BE7D86"
+          label="Price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
         <DropDown
           label="Plant Type"
           options={[{ value: "flower" }, { value: "plant" }]}
@@ -33,10 +44,24 @@ const PostPlantForm = () => {
           <MultipleImageUpload />
         </div>
         <div className="form-section">
-          <DateField fieldColor="#BE7D86" />
-          <TextArea placeholder="Description" minRows={7} minLength={5} />
+          <DateField
+            fieldColor="#BE7D86"
+            onChange={(selectedDate) => setHarvestedDate(selectedDate)}
+          />
+          <TextArea
+            placeholder="Description"
+            minRows={7}
+            minLength={5}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
       </div>
+      <PinkButtonRound
+        label="Submit"
+        onClick={() => {
+          console.log({price, plantName, plantType, harvestedDate, images, description});
+        }}
+      />
     </div>
   );
 };
