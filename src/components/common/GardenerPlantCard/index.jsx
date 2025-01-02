aaed227d814feafe84e65ac54cc5c18e6d7a22d2
Maya Eid <mayabaseleid@gmail.com>
2025-01-02 21:10:53 +0200

    frontend: Add card title, description, price and image

--- src/components/common/GardenerPlantCard/index.jsx
+++ src/components/common/GardenerPlantCard/index.jsx
@@ -4,8 +4,14 @@ import "./style.css";
 const GardenerPlantCard = ({ imageSrc, title, description, price }) => {
   return (
     <div className="card-container">
-      <div className="card-img-container">
-        <img src={imageSrc} alt="Plant" />
+      <div
+        className="card-img-container"
+        style={{ backgroundImage: { imageSrc } }}
+      ></div>
+      <div className="card-info-container">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>From ${price}</p>
       </div>
     </div>
   );
