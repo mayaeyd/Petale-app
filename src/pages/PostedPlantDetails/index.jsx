a67b3fd278a710a6bab50562d4655f1b6fd78720
Maya Eid <mayabaseleid@gmail.com>
2025-01-03 01:32:41 +0200

    frontend: Select plants array from postedPlants slice and filter out the plant selected

--- src/pages/PostedPlantDetails/index.jsx
+++ src/pages/PostedPlantDetails/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./style.css";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PostedPlantDetails = () => {
+  const { id } = useParams();
+  const { postedPlants, loading, error } = useSelector(
+    (state) => state.postedPlants
+  );
+  const plant = postedPlants.find((plant) => plant._id === id);
+  console.log(plant);
+
   return <div></div>;
 };
 
