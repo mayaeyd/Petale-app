eb79d2dd7e957a6e09ad974ffb68b3d511cf1bd2
Maya Eid <mayabaseleid@gmail.com>
2025-01-02 14:28:01 +0200

    frontend: Add postedPlants reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import plantsReducer from "./slices/plantsSlice";
 import flowerRecognitionReducer from "./slices/flowerRecognitionSlice";
+import postedPlantsReducer from "./slices/postedPlantsSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     plants: plantsReducer,
     flower: flowerRecognitionReducer,
+    postedPlants: postedPlantsReducer,
   },
 });
 
