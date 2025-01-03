53dc7987d42acee61e9759e1ba32dee52962a05e
Maya Eid <mayabaseleid@gmail.com>
2025-01-03 13:09:44 +0200

    Destructure the plant's details from postedPlant object in PostedPlantDetails component

--- src/pages/PostedPlantDetails/index.jsx
+++ src/pages/PostedPlantDetails/index.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import "./style.css";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import GardenerNavbar from "../../components/common/GardenerNavbar";
 
 const PostedPlantDetails = () => {
   const { id } = useParams();
   const { postedPlants, loading, error } = useSelector(
     (state) => state.postedPlants
   );
-  const plant = postedPlants.find((plant) => plant._id === id);
-  console.log(plant);
+  const {
+    plantName,
+    plantType,
+    price,
+    quantity,
+    description,
+    harvestDate,
+    images,
+  } = postedPlants.find((plant) => plant._id === id);
 
-  return <div></div>;
+  return (
+    <>
+      <GardenerNavbar />
+    </>
+  );
 };
 
 export default PostedPlantDetails;
