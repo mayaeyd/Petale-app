94563810f0fac24b7e7ff5a166f9795de2267cf6
Maya Eid <mayabaseleid@gmail.com>
2025-01-05 01:40:06 +0200

    frontend: Delete MasonryHero component and all related images and remove it from UserHomePage

--- src/pages/user/UserHomePage/index.jsx
+++ src/pages/user/UserHomePage/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import UserNavbar from "../../../components/common/UserNavbar";
 import Footer from "../../../components/common/Footer";
-import MasonryHero from "../../../components/base/MasonryHero";
 import "./style.css";
 import PinkButtonRound from "../../../components/base/PinkButtonRound";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
@@ -11,7 +10,6 @@ const UserHomePage = () => {
     <div>
       <UserNavbar />
       <div className="hero-section">
-        <MasonryHero />
         <div>
           <h1>PÉTALE</h1>
           <h3>Nurture. Bloom. Connect</h3>
