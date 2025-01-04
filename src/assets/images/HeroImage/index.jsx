d88b4b42fc04d3eeca1bf270067c880443892762
Maya Eid <mayabaseleid@gmail.com>
2025-01-05 01:28:28 +0200

    frontend: Create HeroImage component to use hero image

---
+++ src/assets/images/HeroImage/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import heroImage from "./fleur-pavilia-flower-removebg-preview.png";
+
+const HeroImage = () => {
+  return <img src={heroImage} alt="Hero Flower" width="100%" height="100%" />;
+};
+
+export default HeroImage;
