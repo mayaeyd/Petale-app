bbd28564141c25a77a4cb1e4707a4c3f731a5c0d
Maya Eid <mayabaseleid@gmail.com>
2025-01-05 02:34:18 +0200

    frontend: Use GSAP to create a parallax effect in the user home page hero section

--- src/pages/user/UserHomePage/index.jsx
+++ src/pages/user/UserHomePage/index.jsx
@@ -1,20 +1,84 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import UserNavbar from "../../../components/common/UserNavbar";
 import Footer from "../../../components/common/Footer";
 import "./style.css";
-import PinkButtonRound from "../../../components/base/PinkButtonRound";
-import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import heroimage from "../../../assets/images/HeroImage/hero-image.jpg";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const UserHomePage = () => {
+  const heroRef = useRef(null);
+  const imageRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: heroRef.current,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    tl.to(imageRef.current, {
+      yPercent: 50,
+      ease: "none",
+    }).to(
+      titleRef.current,
+      {
+        yPercent: 100,
+        ease: "none",
+      },
+      0
+    );
+
+    return () => {
+      tl.kill();
+    };
+  }, []);
   return (
     <div>
       <UserNavbar />
-      <div className="hero-section">
-        <div>
-          <h1>PÉTALE</h1>
-          <h3>Nurture. Bloom. Connect</h3>
-        </div>
-      </div>
+      <section
+        ref={heroRef}
+        style={{
+          height: "90vh",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <div
+          ref={imageRef}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backgroundImage: `url(${heroimage})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundColor: "#E4E5E7",
+          }}
+        />
+        <h1
+          ref={titleRef}
+          style={{
+            position: "absolute",
+            width: "100%",
+            textAlign: "center",
+            top: "20%",
+            color: "black",
+          }}
+        >
+          PETALE
+        </h1>
+      </section>
+      <section
+        style={{ height: "100vh", backgroundColor: "#be8d86" }}
+      ></section>
       <Footer />
     </div>
   );
