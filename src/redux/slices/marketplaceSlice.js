11c76b532c2a13ebc95ef14006fa246f0aa776fa
Maya Eid <mayabaseleid@gmail.com>
2025-01-05 16:57:59 +0200

    frontend: Request getAllPosts API in getAllPosts thunk passing auth header

--- src/redux/slices/marketplaceSlice.js
+++ src/redux/slices/marketplaceSlice.js
@@ -1,6 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/marketplace";
+
 const initialState = {
   posts: [],
   loading: false,
@@ -11,6 +13,12 @@ export const getAllPosts = createAsyncThunk(
   "marketplace/getAllPosts",
   async () => {
     try {
+      const response = await axios.get(`${BASE_URL}/`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      return response.data;
     } catch (error) {
       return error.message;
     }
