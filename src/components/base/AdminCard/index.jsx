9916d4490c6c6c1c50a462b718eb662b062c51ff
Maya Eid <mayabaseleid@gmail.com>
2025-01-09 00:33:12 +0200

    frontend: Create AdminCard component to display cards with label, value and icon in dashboard

---
+++ src/components/base/AdminCard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AdminCard = ({ label, value, icon }) => {
+  return (
+    <div className="admin-card-container">
+      <div className="admin-card-icon">{icon}</div>
+      <div>
+        <h2>{value}</h2>
+        <p>{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AdminCard;
