5de551e2a46fd50ec466fac57134023ae2a9d0b3
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 21:10:25 +0200

    frontend: Add authorization header containing bearer token on all http methods for admin

--- src/redux/admin/api/adminApi.js
+++ src/redux/admin/api/adminApi.js
@@ -2,27 +2,40 @@ import axios from "axios";
 
 const BASE_URL = "http://127.0.0.1:8080/admin";
 
+const getAuthHeader = () => ({
+  Authorization: `Bearer ${localStorage.getItem("token")}`,
+});
+
 export const adminApi = {
   get: async (endpoint, id = null) => {
     const response = await axios.get(
-      `${BASE_URL}/${endpoint}${id ? `/${id}` : ""}`
+      `${BASE_URL}/${endpoint}${id ? `/${id}` : ""}`,
+      { headers: getAuthHeader() }
     );
     return response.data;
   },
   post: async (endpoint, data) => {
-    const response = await axios.post(`${BASE_URL}/${endpoint}`, data);
+    const response = await axios.post(`${BASE_URL}/${endpoint}`, data, {
+      headers: getAuthHeader(),
+    });
     return response.data;
   },
   put: async (endpoint, id, data) => {
-    const response = await axios.put(`${BASE_URL}/${endpoint}/${id}`, data);
+    const response = await axios.put(`${BASE_URL}/${endpoint}/${id}`, data, {
+      headers: getAuthHeader(),
+    });
     return response.data;
   },
   delete: async (endpoint, id) => {
-    const response = await axios.delete(`${BASE_URL}/${endpoint}/${id}`);
+    const response = await axios.delete(`${BASE_URL}/${endpoint}/${id}`, {
+      headers: getAuthHeader(),
+    });
     return response.data;
   },
   patch: async (endpoint, id, data) => {
-    const response = await axios.patch(`${BASE_URL}/${endpoint}/${id}`, data);
+    const response = await axios.patch(`${BASE_URL}/${endpoint}/${id}`, data, {
+      headers: getAuthHeader(),
+    });
     return response.data;
   },
 };
