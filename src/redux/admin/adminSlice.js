8a392e3a7dce83cfc813505498683185ec102153
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 16:21:25 +0200

    frontend: Create initialState for each thunk created in adminSlice

---
+++ src/redux/admin/adminSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { userThunks } from "./thunks/userThunks";
+import { postThunks } from "./thunks/postThunks";
+import { orderThunks } from "./thunks/orderThunks";
+import { plantThunks } from "./thunks/plantThunks";
+
+const initialState = {
+  users: {
+    items: [],
+    selectedUser: null,
+    loading: false,
+    error: null,
+  },
+  posts: {
+    items: [],
+    selectedPost: null,
+    loading: false,
+    error: null,
+  },
+  orders: {
+    items: [],
+    selectedOrder: null,
+    loading: false,
+    error: null,
+  },
+  plants: {
+    items: [],
+    selectedPlant: null,
+    loading: false,
+    error: null,
+  },
+  sales: {
+    data: [],
+    loading: false,
+    error: null,
+  },
+};
