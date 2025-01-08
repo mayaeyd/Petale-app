e7bdc205ce9009312fa42b081452020380886cad
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 16:13:27 +0200

    frontend: Add deletePost async thunk for deleting posts via adminApi

--- src/redux/admin/thunks/postThunks.js
+++ src/redux/admin/thunks/postThunks.js
@@ -6,4 +6,8 @@ export const postThunks = {
     "admin/fetchPosts",
     async (id = null) => await adminApi.get("posts", id)
   ),
+  deletePost: createAsyncThunk("admin/deletePost", async (postId) => {
+    const response = await adminApi.delete("posts", postId);
+    return { postId, ...response };
+  }),
 };
