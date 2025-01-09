7c8a4c041b00c08ff4e7626e5cc0356f29e00897
Maya Eid <mayabaseleid@gmail.com>
2025-01-09 22:09:20 +0200

    frontend: Display all listings in cards in admin's marketplace page

--- src/pages/admin/Marketplace/index.jsx
+++ src/pages/admin/Marketplace/index.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import AdminNavbar from "../../../components/common/AdminNavbar";
 import "./style.css";
+import { useSelector } from "react-redux";
+import { selectAllListings } from "../../../redux/admin/adminSlice";
+import GardenerPlantCard from "../../../components/common/GardenerPlantCard";
 
 const Marketplace = () => {
+  const listings = useSelector(selectAllListings);
+  console.log(listings);
+
   return (
     <>
       <AdminNavbar />
+      <div className="marketplace-container">
+        <h1>Marketplace Listings</h1>
+        <div>
+          {listings.map((post) => (
+            <GardenerPlantCard
+              title={post.plantName}
+              price={post.price}
+              description={post.description}
+              imageSrc={post.images[0]}
+              key={post._id}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
