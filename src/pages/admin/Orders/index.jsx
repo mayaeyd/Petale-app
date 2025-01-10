b8394f94d4f89944366fc9b53d775702581e6861
Maya Eid <mayabaseleid@gmail.com>
2025-01-10 15:17:31 +0200

    frontend: Create the rows and headers for the orders table

--- src/pages/admin/Orders/index.jsx
+++ src/pages/admin/Orders/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import AdminNavbar from "../../../components/common/AdminNavbar";
 import "./style.css";
+import { useSelector } from "react-redux";
+import { selectAllOrders } from "../../../redux/admin/adminSlice";
+import SortableTable from "../../../components/common/SortableTable";
+import PinkButtonRound from "../../../components/base/PinkButtonRound";
 
 const Orders = () => {
+  const orders = useSelector(selectAllOrders);
+
+  const headers = [
+    { key: "buyerName", label: "Buyer Name", sortable: true },
+    { key: "buyerEmail", label: "Buyer Email", sortable: true },
+    { key: "numberofOrders", label: "Number of Orders", sortable: true },
+    { key: "address", label: "Buyer Address", sortable: true },
+  ];
+
+  const rows = orders.map((order) => ({
+    buyerName: order.buyerName,
+    buyerEmail: order.buyerEmail,
+    numberofOrders: order.orders.length,
+    address: order.orders[0].buyerAddress,
+    id: order.buyerId,
+  }));
+
   return (
     <>
       <AdminNavbar />
