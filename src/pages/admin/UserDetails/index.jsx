1b68b7a647ab188b2ad5c219bc1750a7435069aa
Maya Eid <mayabaseleid@gmail.com>
2025-01-10 18:40:09 +0200

    frontend: Dispatch fetchUser thunk in a useEffect hook to prevent bugs and select selectedUser from state

--- src/pages/admin/UserDetails/index.jsx
+++ src/pages/admin/UserDetails/index.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userThunks } from "../../../redux/admin/thunks/userThunks";
 import { useParams } from "react-router-dom";
-import { selectUsersLoading } from "../../../redux/admin/adminSlice";
+import {
+  selectSelectedUser,
+  selectUsersLoading,
+} from "../../../redux/admin/adminSlice";
 import { CircularProgress } from "@mui/material";
+import AdminNavbar from "../../../components/common/AdminNavbar";
 
 const UserDetails = () => {
-  const userId = useParams();
+  const { id } = useParams();
   const dispatch = useDispatch();
-  const user = dispatch(userThunks.fetchUsers(userId));
   const loading = useSelector(selectUsersLoading);
-  if (loading) return <CircularProgress />;
+  const user = useSelector(selectSelectedUser);
+
+  useEffect(() => {
+    dispatch(userThunks.fetchUsers(id));
+  }, [dispatch, id]);
+
+  if (loading)
+    return (
+      <div className="spinner-container">
+        <CircularProgress />
+      </div>
+    );
 
   console.log(user);
 
-  return <></>;
+  return (
+    <>
+      <AdminNavbar />
+    </>
+  );
 };
 
 export default UserDetails;
