763a1d582a74849f15b4cab597a8dce94afdd65f
Maya Eid <mayabaseleid@gmail.com>
2025-01-10 13:34:51 +0200

    frontend: Create the headers array and rows objects in GrwoingPlants page

--- src/pages/admin/GrowingPlants/index.jsx
+++ src/pages/admin/GrowingPlants/index.jsx
@@ -4,9 +4,29 @@ import PinkButtonRound from "../../../components/base/PinkButtonRound";
 import "./style.css";
 import { useSelector } from "react-redux";
 import { selectAllPlants } from "../../../redux/admin/adminSlice";
+import SortableTable from "../../../components/common/SortableTable";
 
 const GrowingPlants = () => {
   const gardeners = useSelector(selectAllPlants);
+
+  const headers = [
+    { key: "gardenerName", label: "Gardener Name", sortable: true },
+    { key: "gardenName", label: "Garden Name", sortable: true },
+    { key: "location", label: "Location", sortable: true },
+    { key: "plants", label: "Growing Plants", sortable: true },
+  ];
+
+  const getGrowingPlantsCount = (plants) => {
+    return plants.filter((plant) => !plant.isHarvested).length;
+  };
+
+  const rows = gardeners.map((gardener) => ({
+    gardenerName: gardener.gardenerName,
+    gardenName: gardener.gardenName,
+    location: gardener.location,
+    plants: getGrowingPlantsCount(gardener.plants),
+    id: gardener.gardenerId,
+  }));
 
   return (
     <>
