1b7d0c8a94b037856b03b32c466c7a96e1576009
Maya Eid <mayabaseleid@gmail.com>
2025-01-11 17:03:35 +0200

    frontend: Dispatch fetchPost by ID and select selectedPost and loading

--- src/pages/admin/ListingDetails/index.jsx
+++ src/pages/admin/ListingDetails/index.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdminNavbar from "../../../components/common/AdminNavbar";
 import "./style.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { postThunks } from "../../../redux/admin/thunks/postThunks";
+import {
+  selectPostsLoading,
+  selectSelectedPost,
+} from "../../../redux/admin/adminSlice";
+import { CircularProgress } from "@mui/material";
 
 const ListingDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const loading = useSelector(selectPostsLoading);
+  const post = useSelector(selectSelectedPost);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(postThunks.fetchPosts(id));
+  }, [dispatch, id]);
+
+  if (loading || !post)
+    return (
+      <div className="spinner-container">
+        <CircularProgress />
+      </div>
+    );
+  console.log(post);
+
   return (
     <>
       <AdminNavbar />
