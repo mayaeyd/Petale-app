b71d6a86db680c07b35d825be874e6b9745339c7
Maya Eid <mayabaseleid@gmail.com>
2025-01-14 21:07:26 +0200

    frontend: Add text fields with labels in checkout page

--- src/pages/user/Checkout/index.jsx
+++ src/pages/user/Checkout/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import UserNavbar from "../../../components/common/UserNavbar";
 import "./style.css";
+import CustomInput from "../../../components/base/WhiteTextField";
+import WhiteTextField from "../../../components/base/WhiteTextField";
 
 const Checkout = () => {
   return (
     <>
       <UserNavbar />
+      <div className="checkout-container">
+        <div>
+          <h2>Shipping Information</h2>
+          <div className="checkout-inputs-container">
+            <WhiteTextField label="Address" />
+            <WhiteTextField label="Apartment, suite, etc." />
+          </div>
+          <h2 className="checkout-label">Delivery Method</h2>
+        </div>
+        <div></div>
+      </div>
     </>
   );
 };
