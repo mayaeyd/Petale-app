2bb963fb08cb3d4c3476902261d697ab395af4c7
Maya Eid <mayabaseleid@gmail.com>
2025-01-19 17:49:13 +0200

    frontend: Implement functions to load and save cart state from/to local storage with error handling

---
+++ src/utils/localStorage.js
@@ -0,0 +1,18 @@
+export const loadCartFromLocalStorage = () => {
+  try {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : undefined; // Return saved cart state if it exists
+  } catch (err) {
+    console.error("Could not load cart from local storage", err);
+    return undefined;
+  }
+};
+
+export const saveCartToLocalStorage = (cart) => {
+  try {
+    console.log(cart);
+    localStorage.setItem("cart", JSON.stringify(cart)); // Save cart state to local storage
+  } catch (err) {
+    console.error("Could not save cart to local storage", err);
+  }
+};
